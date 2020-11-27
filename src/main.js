c340ffe5a9634cc264530492c106cf853516065c
Loueye <47384202+loueye@users.noreply.github.com>
2020-11-27 10:10:33 +0100

    V1.3
    Added songs in JSON file that is called in Player instead of directly calling them in an variable array

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import './registerServiceWorker'
+import VueLocalForage from 'vue-localforage'
+
 
 Vue.config.productionTip = false
+Vue.use(VueLocalForage)
 
 new Vue({
   vuetify,
